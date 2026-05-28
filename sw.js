43f18bda6aa96afcb0354ca60e3e9f7c87d8b559
lukaxkax-ops <lukaxkax@gmail.com>
2026-05-28 08:43:44 -0300

    layout: reposicionar botao sair um pouco mais para cima no mobile

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
   Criado com amor para permitir a instalação nativa rápida.
 */
 
-const CACHE_NAME = "fechou-cache-v13";
+const CACHE_NAME = "fechou-cache-v14";
 const ASSETS = [
   "./",
   "./index.html",
