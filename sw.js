84fb556639887ccce3966e7e0a98b532b877243a
lukaxkax-ops <lukaxkax@gmail.com>
2026-05-28 09:02:00 -0300

    layout: fix mobile inputs size and direct child button styling for stacked camera icon

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
   Criado com amor para permitir a instalação nativa rápida.
 */
 
-const CACHE_NAME = "fechou-cache-v19";
+const CACHE_NAME = "fechou-cache-v20";
 const ASSETS = [
   "./",
   "./index.html",
