55f2f926c5d8f70a58bb3717bb3285d21d0fb705
lukaxkax-ops <lukaxkax@gmail.com>
2026-05-28 08:50:41 -0300

    layout: otimizar largura do campo de descricao da despesa no desktop e mobile

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
   Criado com amor para permitir a instalação nativa rápida.
 */
 
-const CACHE_NAME = "fechou-cache-v15";
+const CACHE_NAME = "fechou-cache-v16";
 const ASSETS = [
   "./",
   "./index.html",
